fa651c043e53f948720ac15936afeb306eec98e5
Rose Davidson <rhd@google.com>
2017-10-27 16:33:47 -0700

    Subminor version bump (2.4.0 -> 2.4.1)

--- endpoints/__init__.py
+++ endpoints/__init__.py
@@ -34,4 +34,4 @@ from users_id_token import get_current_user, get_verified_jwt, convert_jwks_uri
 from users_id_token import InvalidGetUserCall
 from users_id_token import SKIP_CLIENT_ID_CHECK
 
-__version__ = '2.4.0'
+__version__ = '2.4.1'
