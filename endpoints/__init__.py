3c1ca517b31bc76c65155d4dab96e53f829c8451
Rose Davidson <rhd@google.com>
2018-02-26 12:13:01 -0800

    Bump major version (2.5.0 -> 3.0.0)
    Rationale: Breaking changes in API naming and versioning
    
    * API names must now match the regexp `[a-z][a-z0-9]{0,39}`. This
      facilitates multiple APIs being allowed in a single service.
    * API versions which are valid semver version strings will be parsed
      and the major version extracted as used in the path. This is a
      change from the previous behavior of using the whole string.

--- endpoints/__init__.py
+++ endpoints/__init__.py
@@ -33,4 +33,4 @@ from .users_id_token import get_current_user, get_verified_jwt, convert_jwks_uri
 from .users_id_token import InvalidGetUserCall
 from .users_id_token import SKIP_CLIENT_ID_CHECK
 
-__version__ = '2.5.0'
+__version__ = '3.0.0'
